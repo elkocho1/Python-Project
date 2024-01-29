96fb10c186b6fcde14b187d967c067f8fdfc2a13
elkocho1 <>
2024-01-29 19:19:24 +0000

    Add gameplan and import libraries

--- run.py
+++ run.py
@@ -1,3 +1,16 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+"""
+Gameplan 
+1. Its a 10x10 Board, with 8 ships of different lengths at random places
+2. 2 Players with 50 shots each 
+3. Players choose their shot coordinates and computer create the coordinate randomly
+4. The Board will update to show misses and hits
+5. Ships are places horizontaly and vertically.
+"""
+
+import random
+import time
+
