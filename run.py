1883776254f6f36aa967c84f11537e69803935ca
elkocho1 <>
2024-01-29 19:49:14 +0000

    Add global variables for a single player

--- run.py
+++ run.py
@@ -11,6 +11,30 @@ Gameplan
 5. Ships are places horizontaly and vertically.
 """
 
+
+
 import random
 import time
 
+"""
+Create the game for a single player 
+Source: https://github.com/SaranSundar/PythonCurriculum/blob/main/Battleships/battleships_empty.py
+This version has been modified and expanded upon by Michael Hirt for the purpose of this Project 3 - Python
+"""
+
+board = [[]]
+board_size = 10
+num_of_ships = 8
+shots_left = 50
+game_over = False
+num_of_ships_sunk = 0
+ship_positions = [[]]
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+def check_ship_placement(start_row, end_row, start_col, end_col):
+    """
+    Check if a ship can be placed in specific area. Returns True if placement is possible otherwise False
+
+    """
+    global board
+    global ship_positions
